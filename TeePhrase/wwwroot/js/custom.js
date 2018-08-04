fd9ab7ba3a23ba6240bba8243aa3cf9789b4c087
Emmanuel Tissera <emmanuel.tissera@getstarted.net.au>
2018-08-04 12:43:41 +1000

    Add project files.

---
+++ TeePhrase/wwwroot/js/custom.js
@@ -0,0 +1,33 @@
+﻿jQuery(document).ready(function ($) {
+
+    if (jQuery().quicksand) {
+
+        // Clone applications to get a second collection
+        var $data = $(".portfolio-area").clone();
+
+        //NOTE: Only filter on the main portfolio page, not on the subcategory pages
+        $('.filter button').click(function (e) {
+            
+            $(".filter button").removeClass("active");
+            // Use the last category class as the category to filter by. This means that multiple categories are not supported (yet)
+            var filterClass = $(this).attr('data-filter');
+            
+            if (filterClass == 'all') {
+                var $filteredData = $data.find('.portfolio-item');
+            } else {
+                var $filteredData = $data.find('.portfolio-item[data-type=' + filterClass + ']');
+            }
+
+            $(".portfolio-area").quicksand($filteredData, {
+                duration: 400,
+                easing: 'easeInOutQuad',
+                adjustHeight: 'auto'
+            });
+            
+            $(this).addClass("active");
+            return false;
+        });
+
+    }//if quicksand
+
+});
